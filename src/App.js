5c426093003dda0e110b97dbb6459fecbfca2767
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-04-25 19:38:17 +0100

    Importei funçao das rotas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
-
-
+import React, { useEffect, useState } from 'react';
+import {isauth} from './services'
+import {Container} from 'react-bootstrap'
+import {ContextProvider} from './context/'
+import Header from './Components/shared/Header';
+import {BrowserRouter as Router} from 'react-router-dom'
+import Routes from './routes';
 function App() {
+   const [userAuth,setUserAuth] = useState({})
+  useEffect(() => {
+    setUserAuth(isauth())
+    console.log(userAuth)
+  },[])
   return (
- <h1>hello</h1>
+    <ContextProvider value={userAuth}>  
+    <Header /> 
+    <Container>
+             <Router>
+                 <Routes />
+             </Router>        
+    </Container>  
+      <h1>{JSON.stringify(userAuth)}</h1>
+    </ContextProvider>
+
   );
 }
 
