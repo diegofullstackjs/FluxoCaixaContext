a8066b66ad17be9d54f088410d3610017c7062f5
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-04-25 19:57:52 +0100

    Resolvendo alguns conflitos

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,13 +2,12 @@ import React, { useState,useEffect } from 'react';
 import {Row,Col,Form,Button} from 'react-bootstrap'
 import {api} from '../../services/index'
 export default function LoginPage() {
-    const [status,setStatus] = useState({});
-    const [form,setform] = useState({
-        email: '',
-        password: ''
-    })
+    const [status,setStatus] = useState({
+        loading: false
+    });
+    const [form,setform] = useState()
 
-    function showMessage(status){
+    function ShowMessage(status){
         console.log(status)
         if(status.type === "error"){
             return (
@@ -21,29 +20,33 @@ export default function LoginPage() {
         }
     }
     useEffect(() => {
-        showMessage(status)
+        ShowMessage(status)
     },[status])
     const Logar = e => {
         e.preventDefault();
-
-        api.post('/user/login',form)
+      setStatus({
+          loading: true
+      })
+      api.post('/user/login',form)
             .then((response) => {
-                if(response.data._id)
+                if(response.data.user)
                 {
                     setStatus({
+                        loading:false,
                         type: "success",
-                        message: "Usuario cadastrado corretamente",
+                        message: "Usuario Logado Corretamente",
                         user: response.data
                     })
                 }
             }).catch((e) => {
                 setStatus({
+                    loading:false,
                     type: "error",
                     messsage: "Ocorreu um erro em nossos servidores"
                 })
             })
     }
-    const onChange = e => {
+    const handleChange = e => {
         setform({
             ...form,
             [e.target.name] : e.target.value
@@ -54,18 +57,20 @@ export default function LoginPage() {
     <Row className="mt-2">
     <Col md={4} className="offset-md-4">
     <Form onSubmit={Logar}>
-         <showMessage />
+         { status.message} 
         <Form.Group controlId="LoginEmail">
             <Form.Label>E-mail</Form.Label>
-            <Form.Control type="email" name="email" onChange={onchange} placeholder="Enter email" />
+            <Form.Control type="email" name="email" onChange={handleChange} placeholder="Enter email" />
         </Form.Group>
 
         <Form.Group controlId="LoginPassword">
             <Form.Label>Senha</Form.Label>
-            <Form.Control type="password" name="password" onChange={onchange} placeholder="Password" />
+            <Form.Control type="password" name="password" onChange={handleChange} placeholder="Password" />
         </Form.Group>
         <Button variant="primary" block type="submit">
-            Entrar
+           {
+               status.loading? 'Carregando' : 'Entrar'
+           }
         </Button>
     </Form>
     </Col>
