33c883c17ff2610aa8a320d10fada0385caa33bc
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-04-25 18:40:06 +0100

    Arquivo para serviçoes e helpers

---
+++ src/services/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+export function isauth() {
+    if(localStorage.getItem('token') && localStorage.getItem('user'))
+    {
+        return {
+            token: localStorage.getItem('token'),
+            user : localStorage.getItem('user')
+        }
+    }
+    return {
+        error: "Usuario nao autenticado"
+    }
+}
+export const api = axios.create({
+    baseURL: "http://www.acompanhantes.io"
+})
