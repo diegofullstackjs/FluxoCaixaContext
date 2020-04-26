015781030993cd078b44ebeb5eba26e356d76c64
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-04-26 02:22:14 +0100

    ja nao lembro oque eu fiz :D

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -10,19 +10,25 @@ import {UserInfo} from '../../context/info'
 
 export default function DashboardPage() {
   const {auth} = useContext(AuthContext)
-  const {data} = useContext(UserInfo)
+  const {data,createCategorie} = useContext(UserInfo)
   useEffect(()=> {
     document.title= `Dashboard ${auth.user}`
   })
   return (
     <Row>
-        <Col md={12}><h1>{data.loading? "Carregando informaçoes" : `Bem vindo ${auth.user}`} </h1></Col>
-        <Col md={4} >
-          <Row>
-              <Col md={12}><ModalCat/></Col>
-              <Col md={12} className="mt-2"><ModalBox /></Col>
-          </Row>
-        </Col>
+        <Col md={12}><h1 className="text-center">{data.loading? "" : `Bem vindo ${auth.user}`} </h1></Col>
+        {
+          data.loading? <h1>Carregando..</h1> :
+          (
+          <Col md={4} className="mt-2" >
+            <Row>
+                <Col md={12}><ModalCat contextCategoria={createCategorie} message={data.msg}/></Col>
+                <Col md={12} className="mt-2"><ModalBox  /></Col>
+            </Row>
+          </Col>
+          )
+        }
+
     </Row>
      
   );
