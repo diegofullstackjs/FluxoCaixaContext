6813a19e04d4e276701147dd51459da8940e7344
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-04-26 01:06:32 +0100

    dando vida a pagina dashboard

---
+++ src/pages/Dashboard/Components/formModalBox.js
@@ -0,0 +1,44 @@
+import React,{useState} from 'react';
+import {  Modal,Form,
+    Button} from 'react-bootstrap'
+// import { Container } from './styles';
+
+export default function ModalBox() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  return (
+    <>
+      <Button variant="primary" block onClick={handleShow}>
+        Criar um novo Caixa
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Criar Novo Caixa</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form>
+            <Form.Group controlId="name">
+              <Form.Label>Caixa</Form.Label>
+              <Form.Control type="text" name="name" placeholder="Pizzaria do gordo" />
+            </Form.Group>
+            <Form.Group controlId="description">
+              <Form.Label>Pequena descriçao</Form.Label>
+              <Form.Control as="textarea" name="description" rows="3" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Fechar
+          </Button>
+          <Button variant="success" onClick={handleClose}>
+            ADCIONAR
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
