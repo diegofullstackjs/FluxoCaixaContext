600beb88faed9305aef8c086c450629e378119a7
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-04-26 02:25:47 +0100

    Removi umas inportaçoes que nao precisava mas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
-import React,{useContext} from 'react';
+import React,{useContext,} from 'react';
 import {AuthContext} from '../context/'
 import DashBoardRoutes from './dashboard.routes';
 import AuthRoutes from './auth.routes';
 const AppRoutes = () => {
     const {auth} = useContext(AuthContext);
+
     return auth? <DashBoardRoutes/> : <AuthRoutes/>
 }
 
